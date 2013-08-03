8ccbefa30218438da7ec7cf6872ce32491daef24
David Siegel <djsiegel@gmail.com>
2013-08-03 13:13:31 -0700

    We don't need to police the reveal gesture angle, somehow it just works.

--- Cassette/RevealMenuGestureRecognizer.cs
+++ Cassette/RevealMenuGestureRecognizer.cs
@@ -14,21 +14,9 @@ namespace Cassette
 
 	class RevealMenuGestureRecognizer : UIPanGestureRecognizer
 	{
-		const int AcceptableAngle = 70;
-
-		bool AngleIsAccpetable (UIGestureRecognizer gesture)
-		{
-			var pan = gesture as UIPanGestureRecognizer;
-			var velocity = pan.VelocityInView (View);
-			var angle = Math.Atan2 (velocity.Y, velocity.X) * 180 / Math.PI;
-			var accept = Math.Abs (angle) < AcceptableAngle || Math.Abs (angle - 180) < AcceptableAngle;
-			return accept;
-		}
-
 		public RevealMenuGestureRecognizer (Action<RevealMenuGestureRecognizer> action) :
 			base (pan => action (pan as RevealMenuGestureRecognizer))
 		{
-			ShouldBegin = AngleIsAccpetable;
 		}
 	}
 	
